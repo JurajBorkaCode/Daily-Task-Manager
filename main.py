25ebf91f7598d81698f32268d8c8df7a5765ffc1
Juraj Borka <jurajborka1999@gmail.com>
2024-02-13 11:03:51 +0000

    Worked on UI
    - added all UI elements needed for new daily creation

--- main.py
+++ main.py
@@ -41,16 +41,65 @@ class MyApp(tk.Tk):
         self.tasks.tag_configure("completed", background="green")
         self.tasks.tag_configure("uncompleted", background="red")
 
+        daily_options_frame = tk.LabelFrame(self, text="Daily Options", labelanchor="nw", width=480, height=50)
+        daily_options_frame.grid(row=1, column=0, columnspan=3)
 
-        open_selected_button = tk.Button(self,text="Open Selected",command=self.open_selected,width=18)
-        open_selected_button.grid(row=1,column=0,padx=(10,10),pady=(0,10))
+        open_selected_button = tk.Button(daily_options_frame,text="Open Selected",command=self.open_selected,width=18)
+        open_selected_button.grid(row=0,column=0,padx=(10,10),pady=(0,10))
 
-        complete_selected_button = tk.Button(self,text="Complete Selected",command=self.complete_selected,width=18)
-        complete_selected_button.grid(row=1,column=1,padx=(10,10),pady=(0,10))
+        complete_selected_button = tk.Button(daily_options_frame,text="Complete Selected",command=self.complete_selected,width=18)
+        complete_selected_button.grid(row=0,column=1,padx=(10,10),pady=(0,10))
 
-        delete_selected_button = tk.Button(self,text="Delete Selected",command=self.delete_selected,width=18)
-        delete_selected_button.grid(row=1,column=2,padx=(10,10),pady=(0,10))
+        delete_selected_button = tk.Button(daily_options_frame,text="Delete Selected",command=self.delete_selected,width=18)
+        delete_selected_button.grid(row=0,column=2,padx=(10,10),pady=(0,10))
 
+        new_daily_frame = tk.LabelFrame(self, text="New Daily", labelanchor="nw", width=480, height=100)
+        new_daily_frame.grid(row=2, column=0, columnspan=3,sticky="ewns",padx=(10,10))
+
+        new_daily_name_label = tk.Label(new_daily_frame,text="Name",width=20)
+        new_daily_name_entry = tk.Entry(new_daily_frame,width=52,selectborderwidth=10)
+
+        new_daily_name_label.grid(row=0,column=0)
+        new_daily_name_entry.grid(row=0,column=1,columnspan=2)
+
+        new_daily_type = tk.Menubutton(new_daily_frame,text="Daily Type", relief=tk.RAISED,width=20)
+        new_daily_type.grid(row=1,column=0)
+
+        new_daily_type.menu = tk.Menu(new_daily_type, tearoff=0)
+        new_daily_type["menu"] = new_daily_type.menu
+
+        self.typeVar = tk.IntVar(self,0)
+
+        new_daily_type.menu.add_radiobutton(label="None",variable=self.typeVar)
+        new_daily_type.menu.add_radiobutton(label="Website",variable=self.typeVar)
+        new_daily_type.menu.add_radiobutton(label="Application",variable=self.typeVar)
+        new_daily_type.grid(row=1,column=0)
+
+        self.shorcutVar = tk.StringVar(self,"Daily Shortcut")
+
+        new_daily_shortcut = tk.Entry(new_daily_frame,width=52,selectborderwidth=10,textvariable=self.shorcutVar)
+        new_daily_shortcut.grid(row=1,column=1,columnspan=2)
+
+        self.alertVar = tk.IntVar()
+        new_daily_alert_checkbox = tk.Checkbutton(new_daily_frame,text="Alert",variable=self.alertVar)
+        new_daily_alert_checkbox.grid(row=2,column=0)
+
+        alert_time_frame = tk.LabelFrame(new_daily_frame,text="Alert Time",width=100,height=40)
+        alert_time_frame.grid(row=2,column=1,sticky="w",pady=(10,10),padx=(10,10))
+
+        alert_time_hour = tk.Spinbox(alert_time_frame, from_=0,to=23,width=2)
+        alert_time_label = tk.Label(alert_time_frame,text=":",width=1)
+        alert_time_min = tk.Spinbox(alert_time_frame, from_=0,to=59,width=2)
+
+        alert_time_hour.grid(row=0,column=0)
+        alert_time_label.grid(row=0,column=1)
+        alert_time_min.grid(row=0,column=2)
+
+        create_button = tk.Button(new_daily_frame,text="Create New Daily",command=self.create_new_daily,width=18)
+        create_button.grid(row=2,column=2)
+
+    def create_new_daily(self):
+        print
 
     def selected_item(self):
         item = self.tasks.focus()
@@ -119,4 +168,5 @@ class MyApp(tk.Tk):
 
 if __name__ == "__main__":
     app = MyApp()
+    app.resizable(False,False)
     app.mainloop()
